f6c673132ea2be3d6b80ddd3032e3d35dbe8996c
yoto88888 <146594953+yoto88888@users.noreply.github.com>
2026-09-13 22:52:38 -0500

    Laboratorio#3-MDI

---
+++ Laboratorio#3 - MDI/Laboratorio#3 - MDI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_3___MDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Form2 ventanaTexto = Application.OpenForms.OfType<Form2>().FirstOrDefault();
+
+            if (ventanaTexto != null)
+            {
+                ventanaTexto.BringToFront();
+                ventanaTexto.Focus();
+                // Si ya existía, volvemos a habilitar el botón de inmediato
+            }
+            else
+            {
+                // si no hay ventana abierta
+                ventanaTexto = new Form2();
+                ventanaTexto.MdiParent = this;
+                ventanaTexto.Show();
+            }
+        }
+    }
+}
---
+++ Laboratorio#3 - MDI/Laboratorio#3 - MDI/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Laboratorio#3 - MDI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laboratorio#3 - MDI")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e53f92c7-4f46-498a-9ba7-4054b49aac74")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
